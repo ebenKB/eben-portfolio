f39d1086b303f20ca9c6c26822a49baec8ff6447
ebenKB <eakbo23@gmail.com>
2020-09-14 22:58:33 +0000

    update config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const withReactSvg = require('next-react-svg')
 const path = require('path')
  
+module.exports={
+  assetPrefix: '/ebenkb.github.io/',
+  basePath: 'ebenkb.github.io/'
+}
+
 module.exports = withReactSvg({
   include: path.resolve(__dirname, 'src/assets/svg'),
   webpack(config, options) {
